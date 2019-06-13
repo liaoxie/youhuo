047190fda16d807d0114528d593502f5ca725bf3
liaoxie <2647687858@qq.com>
2019-06-13 13:42:26 +0800

    gouwuc

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')
-gulp.task('sass',function(){
-    gulp.src('./test.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./css'))
-});
-gulp.watch('./test.scss',['sass'])
